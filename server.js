a82eb12b4def308ded925f7d70cd2026858a82e2
nocnex.hamza <nocnex.hamza@hotmail.com>
2025-07-04 12:13:32 +0500

    Add my file(s)

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js running in Kubernetes!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'healthy' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
